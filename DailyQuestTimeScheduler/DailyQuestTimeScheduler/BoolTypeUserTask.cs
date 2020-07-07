bed3010a9896bfbc1b0fd319209fd0fccfc2da8c
GoodSSenDev <yoodonggeun7@naver.com>
2020-07-07 17:12:43 +1200

    Added Task class and BoolTypeTask
    To make give element and record User data

---
+++ DailyQuestTimeScheduler/DailyQuestTimeScheduler/BoolTypeUserTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyQuestTimeScheduler
+{
+    /// <summary>
+    /// 
+    /// This is BoolType Task that the user can check whether they done the task or not.
+    /// </summary>
+    public class BoolTypeUserTask : UserTask
+    {
+
+        private bool isTaskDone;
+        public bool IsTaskDone
+        {
+            get { return isTaskDone; }
+            set
+            {
+                isTaskDone = value;
+                if(value)
+                {
+                    this.SetCompletionTimeToNow();
+                }
+            }
+        }
+
+
+
+        
+    }
+}
---
+++ DailyQuestTimeScheduler/DailyQuestTimeScheduler/UserTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// This is abstract class for Task that the Users' daily or certain day task to hold the Users' data 
+/// this the task should inherit this interface to be child of task holder.
+/// 
+/// </summay>
+
+namespace DailyQuestTimeScheduler
+{
+    public abstract class UserTask
+    {
+        #region properties
+        DateTime Date { get; set; }
+        DateTime TimeOfCompletionUTC { get; set; }
+        DateTime TimeOfCompletionLocal { get; set; }
+        #endregion
+
+        public UserTask()
+        {
+            this.Date = DateTime.Now;
+        }
+        public UserTask(DateTime dateOfTask)
+        {
+            this.Date = dateOfTask;
+        }
+
+        
+        public void SetCompletionTimeToNow()
+        {
+            TimeOfCompletionUTC = DateTime.UtcNow;
+            TimeOfCompletionLocal = DateTime.Now;
+        }
+    }
+}
