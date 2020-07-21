5ee2cd0dea37f7f1825da0d9a1ffa8ecc9f5b642
GoodSSenDev <yoodonggeun7@naver.com>
2020-07-21 22:46:42 +1200

    Added DelegateCommand class & Some Unit testing from  MainWindowViewModel Testing

---
+++ DailyQuestTimeScheduler/DailyQuestTimeScheduler.Test/MainWindowViewModelTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using DailyQuestTimeScheduler.ViewModels;
+using Moq;
+using Xunit;
+
+
+namespace DailyQuestTimeScheduler.Tests
+{
+    public class MainWindowViewModelTest
+    {
+        MainWindowViewModel mainWindowVM;
+
+        public MainWindowViewModelTest()
+        {
+            mainWindowVM = new MainWindowViewModel();
+        }
+
+        [Fact]
+        public async Task GetTaskHolderList_ShouldGetTheTaskHolderDataFromDB()
+        {
+            var dBAccessClass = new Mock<SqliteDataAccess>();
+            var testTaskHolderExpect = new NormalTaskHolder("Test", "this is testing2", true, 0b01010101, 1, 3214);
+
+
+            dBAccessClass.Setup(x => x.GetTaskHolderListAsync())
+                .ReturnsAsync((new List<NormalTaskHolder>
+                {  new NormalTaskHolder("Test", "this is testing2", true, 0b01010101, 1, 3214)}));
+
+            mainWindowVM = new MainWindowViewModel(dBAccessClass.Object);
+
+            var actual =  await mainWindowVM.GetTaskHolderListAsync();
+
+            Assert.Equal(testTaskHolderExpect.Title, actual[0].Title);
+            Assert.Equal(testTaskHolderExpect.WeeklyRepeatPattern, actual[0].WeeklyRepeatPattern);
+            Assert.Equal(testTaskHolderExpect.Description, actual[0].Description);
+        }
+
+
+    }
+}
---
+++ DailyQuestTimeScheduler/DailyQuestTimeScheduler/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuestTimeScheduler.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public SqliteDataAccess DBAccess { get; set; }
+
+        private ObservableCollection<BoolTypeUserTask> boolTypeTaskList = new ObservableCollection<BoolTypeUserTask>();
+        public ObservableCollection<BoolTypeUserTask> BoolTypeTaskList
+        {
+            get { return boolTypeTaskList; }
+        }
+
+        public List<NormalTaskHolder> TaskHolderList { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.DBAccess = new SqliteDataAccessSqliteCon();
+        }
+
+        public MainWindowViewModel(SqliteDataAccess dBAccess)
+        {
+            this.DBAccess = dBAccess;
+        }
+
+        public async Task<List<NormalTaskHolder>> GetTaskHolderListAsync()
+        {
+            return TaskHolderList = await DBAccess.GetTaskHolderListAsync();
+        }
+
+        // update the task that is checked using command 
+
+    }
+}
